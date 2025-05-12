10ee8686770d95eead07b49cc845ec7890472f71
apeissigma <peissigma@colum.edu>
2025-05-12 11:07:51 -0500

    battleship assign. update

--- battleship/include/user_solver.h
+++ battleship/include/user_solver.h
@@ -6,6 +6,7 @@
 class UserSolver : public Solver {
    public:
     void solve(Board &board) override;
+    void guessNearby(size_t i, size_t j, Board& board); 
 };
 
 #endif // USER_SOLVER_H_
--- battleship/src/user_solver.cpp
+++ battleship/src/user_solver.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
 #include "../include/user_solver.h"
+#include "../include/guesser.h"
+
+int turnCounter = 0; 
+int hitCounter = 0;
 
 void UserSolver::solve(Board &board) {
-    // Your Implementation goes here
-    ReturnType t = guess(1, 1, board);
-    std::cout << t << std::endl;
+    for (size_t i = 0; i < board.getX() && hitCounter < board.getShipCount(); i++) {
+        for (size_t j = 0; j < board.getY() && hitCounter < board.getShipCount(); j++) {
+            if (guess(i, j, board) == hit) {
+                turnCounter++;
+                hitCounter++;
+            } 
+            //if near miss, call guessNearby()
+            else if (guess(i, j, board) == near_miss)
+            {
+                turnCounter++;
+                guessNearby(i, j, board);
+            }
+            else 
+            {
+                turnCounter++;
+            }
+        }
+    }
+    //ReturnType t = guess(1, 1, board);
+    std::cout << turnCounter << std::endl;
+}
+
+//check all adjacent tiles
+void UserSolver::guessNearby(size_t x, size_t y, Board& board)
+{
+    if (x > 0 && guess(x - 1, y, board) == hit)
+    {
+        turnCounter++;
+        hitCounter++;
+    }
+    if (x < board.getX() - 1 && guess(x + 1, y, board) == hit) 
+    {
+        turnCounter++;
+        hitCounter++;
+    }
+    if (y > 0 && guess(x, y + 1, board) == hit) 
+    {
+        turnCounter++;
+        hitCounter++;
+    }
+    if (y < board.getY() - 1 && guess(x, y - 1, board) == hit) 
+    {
+        turnCounter++;
+        hitCounter++;
+    } 
 }
